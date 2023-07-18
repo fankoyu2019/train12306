19a374fa7d07eedb55d7ed3ca2b0420723f63e6e
fankoyu2019 <1402704510@qq.com>
2023-07-18 14:17:11 +0800

    4.4 修改启动日志，增加/member前缀，修改logback日志

--- train-member/src/main/java/com/fanko/train/member/config/MemberApplication.java
+++ train-member/src/main/java/com/fanko/train/member/config/MemberApplication.java
@@ -1,15 +1,22 @@
 package com.fanko.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 @ComponentScan("com.fanko")
 public class MemberApplication {
-
+    private static final Logger Log = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        Log.info("启动成功!!");
+        Log.info("测试地址:\thttp://127.0.0.1:{}/member/hello",env.getProperty("server.port"));
     }
-
 }
