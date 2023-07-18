76baba1f14c94df16ecc622219a22c26ef5458c2
fankoyu2019 <1402704510@qq.com>
2023-07-18 12:39:06 +0800

    Initial commit

---
+++ train-member/src/main/java/com/fanko/train/member/config/MemberApplication.java
@@ -0,0 +1,15 @@
+package com.fanko.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.fanko")
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
