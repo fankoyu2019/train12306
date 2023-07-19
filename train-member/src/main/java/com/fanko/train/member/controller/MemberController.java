c83cd4d1281d2b580fc17b94200e7d899d9083b1
fankoyu2019 <1402704510@qq.com>
2023-07-19 14:24:59 +0800

    4.15 实现简单的注册接口

--- train-member/src/main/java/com/fanko/train/member/controller/MemberController.java
+++ train-member/src/main/java/com/fanko/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.fanko.train.member.controller;
 import com.fanko.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,9 @@ public class MemberController {
     @GetMapping("/count")
     public Integer count(){
         return memberService.count();
+    }
+    @PostMapping("/register")
+    public long register(String mobile){
+        return memberService.register(mobile);
     }
 }
