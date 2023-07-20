32f948b6019870e1484d6c381071a04a5a8073d1
fankoyu2019 <1402704510@qq.com>
2023-07-20 15:35:42 +0800

    5.11 Vue CLI多环境配置；为axios配置后端域名

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -52,7 +52,7 @@ export default defineComponent({
     });
 
     const sendCode = () => {
-      axios.post("http://localhost:8000/member/member/send-code", {
+      axios.post("/member/member/send-code", {
         mobile: loginForm.mobile
       }).then(response => {
         let data = response.data;
@@ -66,7 +66,7 @@ export default defineComponent({
     };
 
     const login = () => {
-      axios.post("http://localhost:8000/member/member/login", loginForm).then(response => {
+      axios.post("/member/member/login", loginForm).then(response => {
         let data = response.data;
         if (data.success) {
           notification.success({description: '登录成功！'});
