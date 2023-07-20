db8df0fd6fc2fe7dbea456be4dcbef793a390ca1
fankoyu2019 <1402704510@qq.com>
2023-07-20 14:27:08 +0800

    5.9 集成axios, npm install axios。发起跨域请求会报跨域错误。

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -1,7 +1,9 @@
 <template>
   <a-row class="login">
     <a-col :span="8" :offset="8" class="login-main">
-      <h1 style="text-align: center"><rocket-two-tone />&nbsp;繁空12306售票系统</h1>
+      <h1 style="text-align: center">
+        <rocket-two-tone/>&nbsp;繁空12306售票系统
+      </h1>
       <a-form
           :model="loginForm"
           name="basic"
@@ -37,7 +39,8 @@
   </a-row>
 </template>
 <script>
-import { defineComponent, reactive } from 'vue';
+import {defineComponent, reactive} from 'vue';
+import axios from "axios";
 
 export default defineComponent({
   name: "login-view",
@@ -47,8 +50,17 @@ export default defineComponent({
       code: '',
     });
 
+    const sendCode = () => {
+      axios.post("http://localhost:8000/member/member/send-code", {
+        mobile: loginForm.mobile
+      }).then(response => {
+        console.log(response);
+      });
+    };
+
     return {
       loginForm,
+      sendCode,
     };
   },
 });
@@ -59,6 +71,7 @@ export default defineComponent({
   font-size: 25px;
   font-weight: bold;
 }
+
 .login-main {
   margin-top: 100px;
   padding: 30px 30px 20px;
