edb7a3c0905a1963f64ea712c78f793beaa53cdd
fankoyu2019 <1402704510@qq.com>
2023-07-20 11:31:21 +0800

    5.7 增加短信验证码接口

--- train-member/src/main/java/com/fanko/train/member/controller/MemberController.java
+++ train-member/src/main/java/com/fanko/train/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.fanko.train.member.controller;
 
 import com.fanko.train.common.resp.CommonResp;
 import com.fanko.train.member.req.MemberRegisterReq;
+import com.fanko.train.member.req.MemberSendCodeReq;
 import com.fanko.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
@@ -15,19 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     @Resource
     private MemberService memberService;
+
     @GetMapping("/count")
-    public CommonResp<Integer> count(){
+    public CommonResp<Integer> count() {
         int count = memberService.count();
         CommonResp<Integer> commonResp = new CommonResp<>();
         commonResp.setContent(count);
         return commonResp;
     }
+
     @PostMapping("/register")
-    public CommonResp<Long> register(@Valid MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req) {
         long register = memberService.register(req);
 //        CommonResp<Long> commonResp = new CommonResp<>();
 //        commonResp.setContent(register);
 //        return commonResp;
         return new CommonResp<>(register);
+    }
+
+    @PostMapping("/send-code")
+    public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req) {
+        memberService.sendCode(req);
+        return new CommonResp<>();
     }
 }
--- train-member/src/main/java/com/fanko/train/member/service/MemberService.java
+++ train-member/src/main/java/com/fanko/train/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.fanko.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.fanko.train.common.exception.BusinessException;
 import com.fanko.train.common.exception.BusinessExceptionEnum;
 import com.fanko.train.common.util.SnowUtil;
@@ -9,25 +10,30 @@ import com.fanko.train.member.domain.Member;
 import com.fanko.train.member.domain.MemberExample;
 import com.fanko.train.member.mapper.MemberMapper;
 import com.fanko.train.member.req.MemberRegisterReq;
+import com.fanko.train.member.req.MemberSendCodeReq;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MemberService {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
     @Resource
     private MemberMapper memberMapper;
-    public int count(){
+
+    public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
     }
 
-    public long register(MemberRegisterReq req){
+    public long register(MemberRegisterReq req) {
         String mobile = req.getMobile();
         MemberExample memberExample = new MemberExample();
         memberExample.createCriteria().andMobileEqualTo(mobile);
         List<Member> list = memberMapper.selectByExample(memberExample);
-        if (CollUtil.isNotEmpty(list)){
+        if (CollUtil.isNotEmpty(list)) {
 //            return list.get(0).getId();
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
@@ -36,5 +42,33 @@ public class MemberService {
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
+    }
+
+    public void sendCode(MemberSendCodeReq req) {
+        String mobile = req.getMobile();
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        // 如果手机号不存在，则插入一条记录
+        if (CollUtil.isEmpty(list)) {
+            LOG.info("手机号不存在，则插入一条记录");
+            Member member = new Member();
+            member.setId(SnowUtil.getSnowflakeNextId());
+            member.setMobile(mobile);
+            memberMapper.insert(member);
+        }else {
+            LOG.info("手机号存在，不插入记录");
+        }
+        // 生成验证码
+//        String code = RandomUtil.randomString(4);
+        String code = "8888";
+        LOG.info("生成短信验证码：{}",code);
+
+        // 保存短信记录表: 手机号， 短信验证码， 有效期， 是否已使用， 业务类型， 发送时间， 使用时间
+        LOG.info("保存短信记录表");
+
+        // 对接短信通道， 发送短信
+        LOG.info("对接短信通道");
+
     }
 }
