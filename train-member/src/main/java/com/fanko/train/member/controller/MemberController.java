c2cd988045c3c44d172fd868b6d43e8a0334180e
fankoyu2019 <1402704510@qq.com>
2023-07-20 14:37:27 +0800

    5.9 后端端口改成body json方式传递参数，前后端联调成功

--- train-member/src/main/java/com/fanko/train/member/controller/MemberController.java
+++ train-member/src/main/java/com/fanko/train/member/controller/MemberController.java
@@ -8,10 +8,7 @@ import com.fanko.train.member.resp.MemberLoginResp;
 import com.fanko.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -37,7 +34,7 @@ public class MemberController {
     }
 
     @PostMapping("/send-code")
-    public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req) {
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
         memberService.sendCode(req);
         return new CommonResp<>();
     }
