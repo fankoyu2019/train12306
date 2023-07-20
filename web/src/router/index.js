6cf0b154d5f5fc88a75c8a554db5b1d58ba4fa1d
fankoyu2019 <1402704510@qq.com>
2023-07-20 10:58:51 +0800

    5.6 修改login页面，登录注册二合一，后端端口未连接

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -14,6 +14,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    component: () => import('../views/login.vue')
   }
 ]
 
