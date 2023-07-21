d6d54cde134ce56e77b058fd7c524303a5a4c021
fankoyu2019 <1402704510@qq.com>
2023-07-21 11:19:50 +0800

    6.9 为axios增加登录拦截:请求时带上token，返回时校验返回码是不是401

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -12,16 +12,18 @@
         <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-          Content
+          所有会员总数： {{count}}
         </a-layout-content>
       </a-layout>
     </a-layout>
   </a-layout>
 </template>
 <script>
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 import TheHeaderView from "@/components/the-header.vue";
 import TheSiderView from "@/components/the-sider.vue";
+import axios from "axios";
+import {notification} from "ant-design-vue";
 
 export default defineComponent({
   components: {
@@ -29,7 +31,18 @@ export default defineComponent({
     TheHeaderView,
   },
   setup() {
-    return {};
+    const count = ref(0);
+    axios.get("/member/member/count").then(response => {
+      let data = response.data;
+      if (data.success) {
+        count.value = data.content;
+      } else {
+        notification.error({description: data.message});
+      }
+    });
+    return {
+      count
+    };
   },
 });
 </script>
