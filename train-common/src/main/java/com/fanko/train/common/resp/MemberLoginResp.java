63cd6c6de88079e020daa2f931c4c01f4bbf0a4f
fankoyu2019 <1402704510@qq.com>
2023-07-21 15:09:58 +0800

    7.5 使用线程本地变量存储会员信息

---
+++ train-common/src/main/java/com/fanko/train/common/resp/MemberLoginResp.java
@@ -0,0 +1,42 @@
+package com.fanko.train.common.resp;
+
+public class MemberLoginResp {
+    private Long id;
+
+    private String mobile;
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("MemberLoginResp{");
+        sb.append("id=").append(id);
+        sb.append(", mobile='").append(mobile).append('\'');
+        sb.append(", token='").append(token).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
