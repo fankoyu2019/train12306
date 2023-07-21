a1cc58bc6b7de64944fe810373bba5798d3a5073
fankoyu2019 <1402704510@qq.com>
2023-07-21 10:35:17 +0800

    6.7 演示gateway过滤器的使用

---
+++ gateway/src/main/java/com/fanko/train/gateway/config/Test1Filter.java
@@ -0,0 +1,26 @@
+package com.fanko.train.gateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class Test1Filter implements GlobalFilter, Ordered {
+    private static final Logger LOG = LoggerFactory.getLogger(Test1Filter.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        LOG.info("Test1Filter");
+        return chain.filter(exchange);
+//        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+}
