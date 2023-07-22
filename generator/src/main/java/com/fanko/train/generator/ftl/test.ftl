dbd67db17e779826fb0521de4c83e0020a70c6c3
fankoyu2019 <1402704510@qq.com>
2023-07-22 17:44:37 +0800

    8.4 集成pom.xml 得到当前持久层的xml文件

---
+++ generator/src/main/java/com/fanko/train/generator/ftl/test.ftl
@@ -0,0 +1,5 @@
+package com.fanko.train.generator.test;
+
+public class ${domain} {
+    private String name;
+}
---
+++ generator/src/main/java/com/fanko/train/generator/server/ServerGenerator.java
@@ -0,0 +1,28 @@
+package com.fanko.train.generator.server;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class ServerGenerator {
+    static String serverPath = "generator/src/main/java/com/fanko/train/generator/";
+    static String pomPath = "generator/pom.xml";
+    static {
+        new File(serverPath).mkdirs();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SAXReader saxReader = new SAXReader();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+
+    }
+}
