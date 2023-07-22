8a89e56dada02dc9df2119ef73a41fe2ee562654
fankoyu2019 <1402704510@qq.com>
2023-07-22 13:50:17 +0800

    7.9 格式化返回的日期字段

--- train-member/src/main/java/com/fanko/train/member/resp/PassengerQueryResp.java
+++ train-member/src/main/java/com/fanko/train/member/resp/PassengerQueryResp.java
@@ -1,5 +1,7 @@
 package com.fanko.train.member.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class PassengerQueryResp {
@@ -12,8 +14,10 @@ public class PassengerQueryResp {
 
     private String type;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
     public Long getMemberId() {
