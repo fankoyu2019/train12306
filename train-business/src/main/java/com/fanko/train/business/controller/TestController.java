7b4854487826bb2870237d7520b8b9d459a70042
fankoyu2019 <1402704510@qq.com>
2023-07-24 19:19:29 +0800

    9.4 增加business业务模块

---
+++ train-business/src/main/java/com/fanko/train/business/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.fanko.train.business.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){return "Hello World! Business";}
+}
