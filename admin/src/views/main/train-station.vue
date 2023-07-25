f01724993628e5de1427583b4e1ff3a47d8cddd4
fankoyu2019 <1402704510@qq.com>
2023-07-25 14:52:18 +0800

    9.13 火车座位页面的车次改为车次选择组件

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -28,11 +28,7 @@
            ok-text="确认" cancel-text="取消">
     <a-form :model="trainStation" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="车次编号">
-        <a-select v-model:value="trainStation.trainCode" show-search :filter-option="filterTrainCodeOption">
-          <a-select-option v-for="item in trains" :key="item.code" :value="item.code" :label="item.code +item.start + item.end">
-            {{item.code}} | {{item.start}} ~ {{item.end}}
-          </a-select-option>
-        </a-select>
+        <train-select-view v-model="trainStation.trainCode" width="50%"></train-select-view>
       </a-form-item>
       <a-form-item label="站序">
         <a-input v-model:value="trainStation.index"/>
@@ -64,9 +60,11 @@ import {defineComponent, ref, onMounted, watch} from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
 import {pinyin} from "pinyin-pro";
+import TrainSelectView from "@/components/train-select.vue";
 
 export default defineComponent({
   name: "train-station-view",
+  components: {TrainSelectView},
   setup() {
     const visible = ref(false);
     let trainStation = ref({
@@ -221,36 +219,11 @@ export default defineComponent({
       });
     };
 
-    // -------------------------车次下拉框--------------------
-    const trains = ref([]);
-    /*
-    * 查询所有的车次，用于车次下拉框
-    */
-    const queryTrainCode = () => {
-      axios.get("/business/admin/train/query-all").then((response) => {
-        let data = response.data;
-        if (data.success) {
-          console.log(data.content);
-          trains.value = data.content;
-        } else {
-          notification.error({description: data.message});
-        }
-      }
-    )
-    }
-    /*
-    * 车次下拉框筛选
-    * */
-    const filterTrainCodeOption = (input, option) =>{
-      console.log(input, option);
-      return option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
-    }
     onMounted(() => {
       handleQuery({
         page: 1,
         size: pagination.value.pageSize
       });
-      queryTrainCode();
     });
 
     return {
@@ -266,8 +239,6 @@ export default defineComponent({
       handleOk,
       onEdit,
       onDelete,
-      filterTrainCodeOption,
-      trains,
     };
   },
 });
