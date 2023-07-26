8f7a67ac4fb567d6be8085cdd9553c36286aaa37
fankoyu2019 <1402704510@qq.com>
2023-07-26 13:58:31 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/fanko/train/batch/job/TestJob.java
+++ batch/src/main/java/com/fanko/train/batch/job/TestJob.java
@@ -1,13 +1,21 @@
 package com.fanko.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("Test Job Test");
+        System.out.println("Test Job Test开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Test Job Test结束");
 
     }
 }
