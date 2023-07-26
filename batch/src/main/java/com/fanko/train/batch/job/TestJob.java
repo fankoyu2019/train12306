434d534a5dd6bdb5662e29843099738b960d55e1
fankoyu2019 <1402704510@qq.com>
2023-07-26 13:54:41 +0800

    10.5 集成quartz 和springboot自带的定时任务可以共存

---
+++ batch/src/main/java/com/fanko/train/batch/job/TestJob.java
@@ -0,0 +1,13 @@
+package com.fanko.train.batch.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TestJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("Test Job Test");
+
+    }
+}
