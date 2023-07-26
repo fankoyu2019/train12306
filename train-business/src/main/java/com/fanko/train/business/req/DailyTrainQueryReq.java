5504aca0ba0149b8113d077027946d8754d22e91
fankoyu2019 <1402704510@qq.com>
2023-07-26 17:41:01 +0800

    11.3 为每日车次页面增加日期和车次查询条件。注意：GET请求的日期是拼接在URL中，需要用Spring自带的@DateTimeFormat(pattern = "yyyy-MM-dd"),后端才能接收到参数

--- train-business/src/main/java/com/fanko/train/business/req/DailyTrainQueryReq.java
+++ train-business/src/main/java/com/fanko/train/business/req/DailyTrainQueryReq.java
@@ -1,12 +1,39 @@
 package com.fanko.train.business.req;
 
 import com.fanko.train.common.req.PageReq;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainQueryReq extends PageReq {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    private String code;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 
     @Override
     public String toString() {
-        return "DailyTrainQueryReq{" +
-                "} " + super.toString();
+        final StringBuffer sb = new StringBuffer("DailyTrainQueryReq{");
+        sb.append("date=").append(date);
+        sb.append(", code='").append(code).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
--- train-business/src/main/java/com/fanko/train/business/req/TrainStationQueryReq.java
+++ train-business/src/main/java/com/fanko/train/business/req/TrainStationQueryReq.java
@@ -3,10 +3,21 @@ package com.fanko.train.business.req;
 import com.fanko.train.common.req.PageReq;
 
 public class TrainStationQueryReq extends PageReq {
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
-        return "TrainStationQueryReq{" +
-                "} " + super.toString();
+        final StringBuffer sb = new StringBuffer("TrainStationQueryReq{");
+        sb.append("trainCode='").append(trainCode).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
