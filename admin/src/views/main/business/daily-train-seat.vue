cc598ea0480e52045d2634e03266f827a876adb9
fankoyu2019 <1402704510@qq.com>
2023-07-28 10:33:18 +0800

    11.6 每日座位界面改为只读

--- admin/src/views/main/business/daily-train-seat.vue
+++ admin/src/views/main/business/daily-train-seat.vue
@@ -2,7 +2,7 @@
   <p>
     <a-space>
       <a-button type="primary" @click="handleQuery()">刷新</a-button>
-      <a-button type="primary" @click="onAdd">新增</a-button>
+      
     </a-space>
   </p>
   <a-table :dataSource="dailyTrainSeats"
@@ -12,15 +12,6 @@
            :loading="loading">
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
-        <a-space>
-          <a-popconfirm
-              title="删除后不可恢复，确认删除?"
-              @confirm="onDelete(record)"
-              ok-text="确认" cancel-text="取消">
-            <a style="color: red">删除</a>
-          </a-popconfirm>
-          <a @click="onEdit(record)">编辑</a>
-        </a-space>
       </template>
       <template v-else-if="column.dataIndex === 'col'">
         <span v-for="item in SEAT_COL_ARRAY" :key="item.code">
@@ -38,43 +29,6 @@
       </template>
     </template>
   </a-table>
-  <a-modal v-model:visible="visible" title="每日座位" @ok="handleOk"
-           ok-text="确认" cancel-text="取消">
-    <a-form :model="dailyTrainSeat" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
-      <a-form-item label="日期">
-        <a-date-picker v-model:value="dailyTrainSeat.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
-      </a-form-item>
-      <a-form-item label="车次编号">
-        <a-input v-model:value="dailyTrainSeat.trainCode" />
-      </a-form-item>
-      <a-form-item label="箱序">
-        <a-input v-model:value="dailyTrainSeat.carriageIndex" />
-      </a-form-item>
-      <a-form-item label="排号">
-        <a-input v-model:value="dailyTrainSeat.row" />
-      </a-form-item>
-      <a-form-item label="列号">
-        <a-select v-model:value="dailyTrainSeat.col">
-          <a-select-option v-for="item in SEAT_COL_ARRAY" :key="item.code" :value="item.code">
-            {{item.desc}}
-          </a-select-option>
-        </a-select>
-      </a-form-item>
-      <a-form-item label="座位类型">
-        <a-select v-model:value="dailyTrainSeat.seatType">
-          <a-select-option v-for="item in SEAT_TYPE_ARRAY" :key="item.code" :value="item.code">
-            {{item.desc}}
-          </a-select-option>
-        </a-select>
-      </a-form-item>
-      <a-form-item label="同车箱座序">
-        <a-input v-model:value="dailyTrainSeat.carriageSeatIndex" />
-      </a-form-item>
-      <a-form-item label="售卖情况">
-        <a-input v-model:value="dailyTrainSeat.sell" />
-      </a-form-item>
-    </a-form>
-  </a-modal>
 </template>
 
 <script>
@@ -150,52 +104,8 @@ export default defineComponent({
       dataIndex: 'sell',
       key: 'sell',
     },
-    {
-      title: '操作',
-      dataIndex: 'operation'
-    }
     ];
 
-    const onAdd = () => {
-      dailyTrainSeat.value = {};
-      visible.value = true;
-    };
-
-    const onEdit = (record) => {
-      dailyTrainSeat.value = window.Tool.copy(record);
-      visible.value = true;
-    };
-
-    const onDelete = (record) => {
-      axios.delete("/business/admin/daily-train-seat/delete/" + record.id).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          notification.success({description: "删除成功！"});
-          handleQuery({
-            page: pagination.value.current,
-            size: pagination.value.pageSize,
-          });
-        } else {
-          notification.error({description: data.message});
-        }
-      });
-    };
-
-    const handleOk = () => {
-      axios.post("/business/admin/daily-train-seat/save", dailyTrainSeat.value).then((response) => {
-        let data = response.data;
-        if (data.success) {
-          notification.success({description: "保存成功！"});
-          visible.value = false;
-          handleQuery({
-            page: pagination.value.current,
-            size: pagination.value.pageSize
-          });
-        } else {
-          notification.error({description: data.message});
-        }
-      });
-    };
 
     const handleQuery = (param) => {
       if (!param) {
@@ -251,10 +161,6 @@ export default defineComponent({
       handleTableChange,
       handleQuery,
       loading,
-      onAdd,
-      handleOk,
-      onEdit,
-      onDelete
     };
   },
 });
