fae1ce442a9d9579c511618aae794f857ccae604
fankoyu2019 <1402704510@qq.com>
2023-07-28 11:06:36 +0800

    11.7 增加生成每日车次定时任务

---
+++ batch/src/main/java/com/fanko/train/batch/job/DailyTrainJob.java
@@ -0,0 +1,24 @@
+package com.fanko.train.batch.job;
+
+import cn.hutool.core.util.RandomUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        //增加流水号
+        MDC.put("LOG_ID",System.currentTimeMillis() + RandomUtil.randomString(3));
+
+        LOG.info("生成每日车次数据开始");
+        LOG.info("生成每日车次数据结束");
+
+    }
+}
