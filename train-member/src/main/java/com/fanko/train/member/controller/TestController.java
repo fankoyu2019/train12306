1d792076048e6570f02f3247167d10c49d75c4ce
fankoyu2019 <1402704510@qq.com>
2023-08-01 16:48:27 +0800

    13.7 Nacos多环境配置，利用命名空间项目隔离，注册中心配置，配置Gateway+Nacos，实现按应用名来做路由转发

--- train-member/src/main/java/com/fanko/train/member/controller/TestController.java
+++ train-member/src/main/java/com/fanko/train/member/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.fanko.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,13 @@ public class TestController {
 
     @Value("${test.nacos}")
     private String testNacos;
+
+    @Autowired
+    Environment environment;
+
     @GetMapping("/hello")
-    public String hello(){return "Hello World" + testNacos;}
+    public String hello() {
+        String port = environment.getProperty("local.server.port");
+        return "Hello World " + "端口 " + port + " " + testNacos;
+    }
 }
