748574fc4379b124d7a30400f5faeb1eff17a860
fankoyu2019 <1402704510@qq.com>
2023-08-03 16:08:30 +0800

    17.2 使用Sentinal进行限流降级，熔断降级, 引入依赖，限流初体验

--- train-business/src/main/java/com/fanko/train/business/config/BusinessApplication.java
+++ train-business/src/main/java/com/fanko/train/business/config/BusinessApplication.java
@@ -1,5 +1,8 @@
 package com.fanko.train.business.config;
 
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +12,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -23,5 +29,20 @@ public class BusinessApplication {
         Environment env = app.run(args).getEnvironment();
         Log.info("启动成功!!");
         Log.info("测试地址:\thttp://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
+        // 限流规则
+        initFlowRules();
+        Log.info("已定义限流规则");
+    }
+
+
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("doConfirm");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(1);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
     }
 }
--- train-business/src/main/java/com/fanko/train/business/service/ConfirmOrderService.java
+++ train-business/src/main/java/com/fanko/train/business/service/ConfirmOrderService.java
@@ -7,6 +7,8 @@ import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import com.fanko.train.business.domain.*;
 import com.fanko.train.business.enums.ConfirmOrderStatusEnum;
@@ -102,6 +104,7 @@ public class ConfirmOrderService {
         confirmOrderMapper.deleteByPrimaryKey(id);
     }
 
+    @SentinelResource(value = "doConfirm",blockHandler = "doConfirmBlock")
     public void doConfirm(ConfirmOrderDoReq req) {
         String lockKey = req.getDate() + "-" + req.getTrainCode();
 //        Boolean setIfAbsent = redisTemplate.opsForValue().setIfAbsent(lockKey, lockKey, 5, TimeUnit.SECONDS);
@@ -428,5 +431,12 @@ public class ConfirmOrderService {
                 }
             }
         }
+    }
+    /*
+    降级方法，需包含限流方法的所有参数和BlackException参数
+    * */
+    public void doConfirmBlock(ConfirmOrderDoReq req, BlockException e){
+        LOG.info("购票请求被限流：{}",req);
+        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
     }
 }
